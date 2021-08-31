a041a7e13d4ba36821f23741dc26449eecde8e0b
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-08-31 11:14:37 +0530

    Created Orders Module

---
+++ kheticulture/orders/serializers.py
@@ -0,0 +1,24 @@
+from . import models
+from .models import Order
+from .models import OrderItem
+from rest_framework import serializers
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=user.objects.all(),
+                                                  many=False)
+    
+    class Meta:
+        model = Order # this is the model that is being serialized
+        fields = ('user','full_name','total_paid',
+                        'order_key','payment_option','billing_status','order_status')
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    order = serializers.PrimaryKeyRelatedField(queryset=order.objects.all(),
+                                                  many=False)
+    
+    product = serializers.PrimaryKeyRelatedField(queryset=product.objects.all(),
+                                                  many=False)
+        
+    class Meta:
+            model = Order # this is the model that is being serialized
+            fields = ('order','product','price','quantity')
