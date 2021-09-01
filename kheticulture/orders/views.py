e69ba5848e11f1c8ea0ef3bca74497ecd09d0c1e
Aastha Mahindra <69888214+aastha-m125@users.noreply.github.com>
2021-09-01 11:45:10 +0530

    Updated Order Module

--- kheticulture/orders/views.py
+++ kheticulture/orders/views.py
@@ -1,45 +1,44 @@
-from django.http.response import JsonResponse
-from .serializers import OrderSerializer, OrderItemSerializer
-from django.shortcuts import get_object_or_404, render
-from tractor.models import Tractor
-from .models import Order, OrderItem
-from rest_framework.decorators import api_view
-from django.contrib.gis.measure import Distance  
-from django.contrib.gis.measure import Distance  
-from rest_framework import serializers, status
-from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from django.contrib.gis.geos import Point
-
-def add(request):
-    #basket = Basket(request)
-    tractor=Tractor(request)
-    if request.POST.get('action') == 'post':
-
-        order_key = request.POST.get('order_key')
-        user_id = request.user.id
-        baskettotal = basket.get_total_price()
-
-        # Check if order exists
-        if Order.objects.filter(order_key=order_key).exists():
-            pass
-        else:
-            order = Order.objects.create(user_id=user_id, full_name='name', address1='add1',
-                                address2='add2', total_paid=baskettotal, order_key=order_key)
-            order_id = order.pk
-
-            #for item in basket:
-            OrderItem.objects.create(order_id=order_id, product=tractor, price='', quantity=1)
-
-        response = JsonResponse({'success': 'Return something'})
-        return response
+from rest_framework import viewsets
+from rest_framework import exceptions
+from orders.models import Order, OrderItem
+from orders.serializers import (OrderDetailCreateSerializer, 
+                    OrderDetailRetrieveSerializer, 
+                    OrderRetrieveSerializer, 
+                    OrderCreateSerializer, 
+                    OrderDetailUpdateSerializer,
+                    OrderStatusRetrieveSerializer,
+                    OrderStatusUpdateSerializer)
 
 
-def payment_confirmation(data):
-    Order.objects.filter(order_key=data).update(billing_status=True)
+class CreateOrder(viewsets.ModelViewSet):
+    
+    queryset = Order.objects.all()
+    filter_fields = ('id', 'number', 'customer', 'status')
 
+    def get_serializer_class(self):
+        method = self.request.method
+        serializer_class = OrderRetrieveSerializer
+        if method == 'POST':
+            serializer_class = OrderCreateSerializer
+        return serializer_class
 
-def user_orders(request):
-    user_id = request.user.id
-    orders = Order.objects.filter(user_id=user_id).filter(billing_status=True)
-    return orders
+    def perform_create(self, serializer):
+        """
+        We need to override this method to add order details after a order
+        is placed. Also note that extra validation is added to ensure order
+        details come in the json data.
+        """
+        details = self.request.data.get('details')
+
+        if not details:
+            raise exceptions.ValidationError({
+                'details': 'Please provide details for this order'
+            })
+
+        order_details_serialized = OrderDetailCreateSerializer(
+            data=details, many=True)
+
+        order_details_serialized.is_valid(raise_exception=True)
+
+        order = serializer.save()
+        order_details_serialized.save(order=order)
