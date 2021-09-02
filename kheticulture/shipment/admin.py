53850ce3e60953dda2039eafb728bba5ff589b13
nandinichhajed <nandinichhajed08@gmail.com>
2021-09-02 23:40:33 +0530

    updating shipment app

--- kheticulture/shipment/admin.py
+++ kheticulture/shipment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Shipment, Shippment_Tracking
 
-# Register your models here.
+admin.site.register(Shipment)
+admin.site.register(Shippment_Tracking)
---
+++ kheticulture/shipment/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+app_name = 'tractor'
+
+urlpatterns = [
+    # path('', views.all_tractors, name='all_tractors'),
+    # path('add/', views.add, name='add'),
+    # path('radiustractor/', views.get_all_tractor_within_radius),
+    # path('availabletractor/', views.get_available_tractors_within_radius),
+]
