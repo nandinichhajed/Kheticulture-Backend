0f051f3cd6c4346c4e5a42d26d4a82839412f45a
nandinichhajed <nandinichhajed08@gmail.com>
2021-10-03 11:04:35 +0530

    Updating job module

--- kheticulture/job/urls.py
+++ kheticulture/job/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
-app_name = 'tractor'
+app_name = 'job'
 
 urlpatterns = [
-    # path('', views.all_tractors, name='all_tractors'),
-    # path('add/', views.add, name='add'),
-    # path('radiustractor/', views.get_all_tractor_within_radius),
-    # path('availabletractor/', views.get_available_tractors_within_radius),
+    path('createjob/', views.create_job, name='createjob'),
+    path('updatejob/', views.update_job, name='updatejob'),
+    path('postinvoice/', views.post_invoice, name='postinvoice'),
 ]
--- kheticulture/job/views.py
+++ kheticulture/job/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
-from .models import Job, JobImage, JobImage
+from .models import Job, JobImage
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.shortcuts import render
+from rest_framework.parsers import JSONParser,ParseError
 
-def create_job(request):
-    pass
+def create_job(APIView):
+    def get(self, request):
+        job = Job.objects.all()
+        serializer = JobSerializer(Job, many = True)
+        return JsonResponse(serializer.data)
+
+    def post(self, request):
+        job = request.data
+        serializer = JobSerializer(data = job)
+        if serializer.is_valid(raise_exception=True):
+            job_saved = serializer.save()
+        return JsonResponse({"success": "Job '{}' Done successfully".format(job_saved.order_key)})
 
 def update_job(request):
-    pass
+    key = Job.objects.get(order_key = order_key)
+    serializer = JobStatusSerializer(instance=key, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return JsonResponse(serializer.data)
 
 def post_invoice(request):
-    pass
+    invoice = request.data
+    serializer = JobSerializer(data = invoice)
+    if serializer.is_valid(raise_exception=True):
+        Total_Cost = serializer.save()
+        return JsonResponse({"success": "Total cost of the order is '{}' rs".format(Total_Cost.order_key)})
